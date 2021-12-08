5dfa733f650eadc79f30a3c1846d438570d0187e
JShweiri <wafflemanz3000@gmail.com>
2021-12-07 23:30:05 -0800

    more changes

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ let ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
+const CIRCLE_SIZE = 90;
+
 let board = [
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
@@ -13,8 +15,6 @@ let board = [
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
 ];
-
-const SIZE = 90;
 
 player = 1;
 
@@ -38,16 +38,19 @@ function doMove(player, x) {
     if(checkWin(board, player)){
         ctx.fillStyle = "black";
         ctx.font = "30px Arial";
-        ctx.fillText("Player " + player +" wins!", SIZE*4 - 100, 24);
+        ctx.fillText("Player " + player +" wins!", CIRCLE_SIZE*4 - 100, 24);
     }
 }
 
-function drawBoard(){
+function drawBoard() {
+    ctx.fillStyle = "lightblue";
+    ctx.rect(0, 0, CIRCLE_SIZE*8, CIRCLE_SIZE*7);
+    ctx.fill();
     for(let i = 0; i < board.length; i++){
         for(let j = 0; j < board[0].length; j++){
            
             ctx.beginPath();
-            ctx.arc((j+1)*SIZE, (i+1)*SIZE, SIZE/2, 0, 2 * Math.PI);
+            ctx.arc((j+1)*CIRCLE_SIZE, (i+1)*CIRCLE_SIZE, CIRCLE_SIZE/2, 0, 2 * Math.PI);
 
             if(board[i][j] == 0){
                 ctx.fillStyle = "gray";
@@ -92,7 +95,7 @@ function oncliq(event) {
     //let y = event.pageY - canvasTop;
 
 
-    doMove(player, Math.trunc((x-SIZE/2)/SIZE));
+    doMove(player, Math.trunc((x-CIRCLE_SIZE/2)/CIRCLE_SIZE));
 
     //switch
     player+=1;
