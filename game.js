48e447e545fc2f1b70d740f72fe0dfd8244fc065
JShweiri <wafflemanz3000@gmail.com>
2021-12-24 00:46:15 -0800

    flexible size

--- game.js
+++ game.js
@@ -22,6 +22,10 @@ canvas.height = CIRCLE_SIZE * (ROW_COUNT+1);
 
 let board = createBoard(ROW_COUNT, COLUMN_COUNT);
 
+function min(a, b) {
+    return a < b ? a : b;
+}
+
 function createBoard(rows, cols) {
     return Array.from({length: rows}, () => new Array(cols).fill(0));
   };
@@ -74,7 +78,7 @@ function doMove1Player(x) {
     if(checkWin(board, player)){
         ctx.fillStyle = "black";
         ctx.font = "30px Arial";
-        ctx.fillText("Player " + player +" wins!", CIRCLE_SIZE*4, 30);
+        ctx.fillText("Player " + player +" wins!", CIRCLE_SIZE * (COLUMN_COUNT+1)/2, 30);
         return;
     }
 
@@ -85,7 +89,7 @@ function doMove1Player(x) {
   ctx.textAlign = "center";
   ctx.fillStyle = "black";
   ctx.font = "30px Arial";
-  ctx.fillText("Thinking..", CIRCLE_SIZE * 4, CIRCLE_SIZE * 6.5 + 30);
+  ctx.fillText("Thinking..", CIRCLE_SIZE * (COLUMN_COUNT+1)/2, CIRCLE_SIZE * (ROW_COUNT+0.5) + 30);
 
   setTimeout(() => {
 
@@ -99,7 +103,7 @@ function doMove1Player(x) {
     if(checkWin(board, player)){
         ctx.fillStyle = "black";
         ctx.font = "30px Arial";
-        ctx.fillText("Player " + player +" wins!", CIRCLE_SIZE*4, 30);
+        ctx.fillText("Player " + player +" wins!", CIRCLE_SIZE * (COLUMN_COUNT+1)/2, 30);
         return;
     }
 
@@ -129,7 +133,7 @@ function doMove2Player(x) {
 
 function drawBoard() {
     ctx.fillStyle = "lightblue";
-    ctx.rect(0, 0, CIRCLE_SIZE*8, CIRCLE_SIZE*7);
+    ctx.rect(0, 0, CIRCLE_SIZE*(COLUMN_COUNT+1), CIRCLE_SIZE*(ROW_COUNT+1));
     ctx.fill();
     for(let i = 0; i < board.length; i++){
         for(let j = 0; j < board[0].length; j++){
@@ -162,10 +166,10 @@ function isTerminalNode(board){
 
 function positionScore(board, player) {
     let score = 0;
-    for (let x = 0; x < 7; x++) {
-        for (let y = 0; y < 6; y++) {
-            for (let n = 1; n < 6; n++) {
-                if (board[y][x] == player && ((y + n) < 6) && ((x + n) < 7)) {
+    for (let x = 0; x < COLUMN_COUNT; x++) {
+        for (let y = 0; y < ROW_COUNT; y++) {
+            for (let n = 1; n < min(COLUMN_COUNT, ROW_COUNT); n++) {
+                if (board[y][x] == player && ((y + n) < ROW_COUNT) && ((x + n) < COLUMN_COUNT)) {
                     score += evaluateSquare([board[y][x], board[y][x + n], board[y + n][x], board[y + n][x + n]], player);
                 }
             }
@@ -208,10 +212,10 @@ return score
 }
 
 function checkWin(board, player){
-    for (let x = 0; x < 7; x++) {
-        for (let y = 0; y < 6; y++) {
-            for (let n = 1; n < 6; n++){
-                if (board[y][x] == player && ((y + n) < 6) && ((x + n) < 7)){
+    for (let x = 0; x < COLUMN_COUNT; x++) {
+        for (let y = 0; y < ROW_COUNT; y++) {
+            for (let n = 1; n < min(COLUMN_COUNT, ROW_COUNT); n++) {
+                if (board[y][x] == player && ((y + n) < ROW_COUNT) && ((x + n) < COLUMN_COUNT)) {
                     if (board[y][x] == player && board[y][x] == board[y + n][x] && board[y][x] == board[y][x + n] && board[y][x] == board[y + n][x + n]) {
                         return true;
                     }
